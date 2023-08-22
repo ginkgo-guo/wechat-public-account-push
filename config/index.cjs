7fd6761b56a237ab9c2c28d88a053869f816d58a
ginkgo-guo <77090357+ginkgo-guo@users.noreply.github.com>
2023-08-21 21:39:07 -0400

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,8 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+// 请将以下代码按照图示粘贴到对应位置
+USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxbb138fae8ff3ae84',
 
@@ -21,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogIP-6qE4nrDjH3RgHk0EVTeMMHo',
+      id: 'PDU24968T1PfHOH8bMRTmZEelrwqWiGJ2Gcw5hevQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'RWJLKwc7PTehlTeCNic67sw2kiXBCBsAkOqr3eJOP9s',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-22',
       festivals: [
@@ -42,13 +43,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'JEOumsPHOAPBBTBB5YBkVa6N32xK159WtEFIast4c6U',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogIP-6qzRobta6I2AAEOLNn6-Cz4',
+      id: 'PDU24968T1PfHOH8bMRTmZEelrwqWiGJ2Gcw5hevQ',
     }
   ],
 
